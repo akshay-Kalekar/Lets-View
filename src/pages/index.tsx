ba9ed5eea51f88d244c2bb3f0a427b83b4da5561
Akshay <akalekar2003@gmail.com>
2023-02-01 01:17:38 +0530

    Figure out SDK wworking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,19 @@ import Search from 'components/search';
 const inter = Inter({ subsets: ['latin'] })
 
 
+console.log("Key is ",)
 const settings = {
-  apiKey: process.env.ALCHEMY_API_KEY,
+  apiKey: 'Fwm3aqGn0c6mK1pnDExDf4pEwHWPGU5q',
   network: Network.ETH_MAINNET,
 };
-
+console.log("settings",settings)
 const alchemy = new Alchemy(settings);
 
 
 export default function Home() {
 
   const [blockNumber, setBlockNumber] = useState<any>();
+  const [find, setFind] = useState<any>();
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -28,7 +30,7 @@ export default function Home() {
     }
 
     getBlockNumber();
-  });
+  },[find]);
 
   return (
     <>
@@ -38,7 +40,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Search/>
+      <Search />
 
       <div className="App">Block Number: {blockNumber}</div>
     </>
